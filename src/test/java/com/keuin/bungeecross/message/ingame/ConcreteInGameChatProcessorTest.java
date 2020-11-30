866e8010b966a8214b8a520945b39f9520b5dc5e
Keuin <keuinx@gmail.com>
2020-12-01 00:13:33 +0800

    Add test cases for ConcreteInGameChatProcessor

--- src/test/java/com/keuin/bungeecross/message/ingame/ConcreteInGameChatProcessorTest.java
+++ src/test/java/com/keuin/bungeecross/message/ingame/ConcreteInGameChatProcessorTest.java
@@ -12,18 +12,28 @@ public class ConcreteInGameChatProcessorTest extends TestCase {
     private final TestableRepeater outboundRepeater = new TestableRepeater();
     private final TestableInstructionDispatcher instructionDispatcher = new TestableInstructionDispatcher();
 
-    private final String repeatMessagePrefix = "98^iuho786I^FG7667";
-    private final String inGameCommandPrefix = "8^89y7&*7GF5$%57d";
+    private String repeatMessagePrefix = "98^iuho786I^FG7667";
+    private String inGameCommandPrefix = "8^89y7&*7GF5$%57d";
 
     private final int sleepMillis = 1000;
 
-    private final ConcreteInGameChatProcessor processor = new ConcreteInGameChatProcessor(
-            repeatMessagePrefix,
-            inGameCommandPrefix,
-            inGameRepeater,
-            outboundRepeater,
-            instructionDispatcher
-    );
+    private ConcreteInGameChatProcessor processor;
+
+    private void initChatProcessor() {
+        processor = new ConcreteInGameChatProcessor(
+                repeatMessagePrefix,
+                inGameCommandPrefix,
+                inGameRepeater,
+                outboundRepeater,
+                instructionDispatcher
+        );
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        initChatProcessor();
+    }
 
     public void tearDown() throws Exception {
         processor.close();
@@ -59,7 +69,48 @@ public class ConcreteInGameChatProcessorTest extends TestCase {
         assertTrue(instructionDispatcher.getCommandList().toString(), instructionDispatcher.getCommandList().contains(commandString));
     }
 
+    public void testIssueCommand2() {
+        inGameCommandPrefix = "!";
+        initChatProcessor();
+
+        UUID uuid = UUID.randomUUID();
+        String commandString = "command";
+        TestableInGameMessage message1 = TestableInGameMessage.create(
+                inGameCommandPrefix + commandString,
+                TestableMessageUser.create("user", uuid, "id", "location"),
+                SkeletonProxiedPlayer.create()
+        );
+        processor.issue(message1);
+        sleep();
+        assertTrue(inGameRepeater.getMessageList().isEmpty());
+        assertTrue(outboundRepeater.getMessageList().isEmpty());
+        assertTrue(instructionDispatcher.getCommandList().toString(), instructionDispatcher.getCommandList().contains(commandString));
+    }
+
     public void testIssueOutboundMessage() {
+        UUID uuid = UUID.randomUUID();
+        String messageString = "message";
+        TestableInGameMessage message1 = TestableInGameMessage.create(
+                repeatMessagePrefix + messageString,
+                TestableMessageUser.create("user", uuid, "id", "location"),
+                SkeletonProxiedPlayer.create()
+        );
+        processor.issue(message1);
+        sleep();
+        assertEquals(1, inGameRepeater.getMessageList().size());
+        assertTrue(instructionDispatcher.getCommandList().isEmpty());
+        boolean contains = false;
+        for (Message m : outboundRepeater.getMessageList()) {
+            if (m.getMessage().equals(messageString))
+                contains = true;
+        }
+        assertTrue(contains);
+    }
+
+    public void testIssueOutboundMessage2() {
+        repeatMessagePrefix = "#";
+        initChatProcessor();
+
         UUID uuid = UUID.randomUUID();
         String messageString = "message";
         TestableInGameMessage message1 = TestableInGameMessage.create(
